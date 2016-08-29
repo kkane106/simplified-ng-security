a9a7d38d69c540a63c086110a71ef7c535a9307a
Kris Kane <kkane106@gmail.com>
2016-08-29 11:38:59 -0600

    Reduce auth controller to bare necessities
    remove advanced error handling for the sake of simplicity. The initial goal is to illustrate the use of jwts, optimization can come in a later section.

--- SimpleNgSecurity/src/controllers/AuthenticationController.java
+++ SimpleNgSecurity/src/controllers/AuthenticationController.java
@@ -1,33 +1,8 @@
 package controllers;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.persistence.NoResultException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import data.UserDao;
-import entities.User;
-import models.JsonWebTokenGenerator;
-import security.SecretKeyGenerator;
-
 @RestController
 @RequestMapping("/auth")
-public class AuthenticationController {
+public class AuthenticationController { 
   @Autowired
   JsonWebTokenGenerator jwtGen;
 
@@ -36,61 +11,37 @@ public class AuthenticationController {
   
   @RequestMapping(value = "/login", method = RequestMethod.POST)
   public Map<String,String> login(HttpServletRequest req, HttpServletResponse res, @RequestBody String userJsonString) {
-    Map<String, String> responseJson = new HashMap<>();
     ObjectMapper mapper = new ObjectMapper();
     User user = null;
     try {
       user = mapper.readValue(userJsonString, User.class);
     } catch (Exception e) {
       e.printStackTrace();
-      res.setStatus(422);
-      responseJson.put("error", "Client data was malformed and unparseable");
-      return responseJson;
     }
     try {
       user = userDao.authenticateUser(user);
-    } catch (NoResultException e) {
-      responseJson.put("error", "Username not found");
-      res.setStatus(404);
-      return responseJson;
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    if (user == null) {
-      responseJson.put("error", "Password/Username combination is incorrect");
-      res.setStatus(401);
-      return responseJson;
-    }
-    if (user != null) {
-      String jws = jwtGen.generateUserJwt(user);
-      responseJson.put("jwt", jws);
-      return responseJson;
-    }
-    // Code may be unreacheable
-    res.setStatus(500);
-    responseJson.put("error", "Internal Server Error");
+    String jws = jwtGen.generateUserJwt(user);
+    Map<String, String> responseJson = new HashMap<>();
+    responseJson.put("jwt", jws);
     return responseJson;
   }
 
   @RequestMapping(value = "/signup", method = RequestMethod.POST)
   public Map<String,String> signup(HttpServletRequest req, HttpServletResponse res, @RequestBody String userJson) {
-    Map<String,String> responseJson = new HashMap<>();
     ObjectMapper mapper = new ObjectMapper();
     User user = null;
     try {
       user = mapper.readValue(userJson, User.class);
     } catch (IOException ie) {
       ie.printStackTrace();
-      res.setStatus(422);
-      responseJson.put("error", "Client data was malformed and unparseable");
-      return responseJson;
     }
     user = userDao.create(user);
-    if (user != null) {
-      String jws = jwtGen.generateUserJwt(user);
-      responseJson.put("jwt", jws);
-      return responseJson;
-    }
-    res.setStatus(500);
-    responseJson.put("error", "Internal Server Error");
+    String jws = jwtGen.generateUserJwt(user);
+    Map<String,String> responseJson = new HashMap<>();
+    responseJson.put("jwt", jws);
     return responseJson;
   }
 }
